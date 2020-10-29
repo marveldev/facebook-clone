2832ba6bd514bd7b72f0d5b509288e3669b643a0
Marvel Dev <marvellousalika48@gmail.com>
2020-10-29 13:12:35 +0100

    - add edit modal

--- modules/mainContent/MainContent.js
+++ modules/mainContent/MainContent.js
@@ -7,7 +7,7 @@ const MainContent = () => {
             <img src="https://images.pexels.com/photos/5031633/pexels-photo-5031633.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             class="profile-photo bio-photo" alt="my profile picture">
           </a>
-          <textarea id="user-post" placeholder="What's on your mind?"></textarea>
+          <textarea id="postInput" placeholder="What's on your mind?"></textarea>
         </div>
         <div class="chat-options">
           <a href="#"><i class="material-icons">&#xe413;</i>Photo/Video</a>
--- modules/mainContent/userEntry.js
+++ modules/mainContent/userEntry.js
@@ -1,14 +1,13 @@
 import { getEntryFromDb } from '../../dataStorage.js';
 import { displayPostOptions } from './events.js';
 
-
 const getPostItemsFromDb = async () => {
   const output = document.querySelector('.output');
   const bioEntry = await getEntryFromDb('bio');
   const userEntry = await getEntryFromDb('post-item');
   const userEntryItems = userEntry.reverse().map((singleEntry) => {
     return `  
-      <div class="post-container">
+      <section class="post-container" id=${singleEntry.postItemId}>
         <div class="post-content"> 
           <div class="post-entry">
             <a href="#">
@@ -17,12 +16,12 @@ const getPostItemsFromDb = async () => {
             </a>
             <strong class="user-name">${bioEntry[0] ? bioEntry[0].bioName : 'Jane Doe'}</strong>
           </div>
-          <p class="user-post">${singleEntry.post}</p>
+          <p id="userText" class=${singleEntry.modalId}>${singleEntry.userPost}</p>
           <div class="edit-post-buttons">
             <button class="display-options-button">...</button>
             <div class="post-item-options">
-              <button class="edit-text-button">EDIT TEXT</button>
-              <button class="delete-post-button">DELETE POST</button>
+              <button class="edit-text-button" title=${singleEntry.modalId}>EDIT TEXT</button>
+              <button class="delete-post-button" title=${singleEntry.postItemId}>DELETE POST</button>
             </div>
           </div>
         </div>
@@ -36,7 +35,24 @@ const getPostItemsFromDb = async () => {
             <a href="#"><i class="fa fa-share"></i>Share</a>
           </div>
         </div>
-      </div>   
+        <div class="edit-post-modal" id=${singleEntry.modalId}>
+          <div class="edit-post">
+            <strong>Edit Post</strong>
+            <button id="closePostButton">X</button>
+          </div>
+          <div id="userInfo"> 
+            <a href="#">
+              <img src="${bioEntry[0] ? bioEntry[0].photoSource : 'https://images.pexels.com/photos/5031633/pexels-photo-5031633.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'}"
+              class="profile-photo bio-photo" alt="my profile picture">
+            </a>
+            <strong id="displayName" class="user-name">${bioEntry[0] ? bioEntry[0].bioName : 'Jane Doe'}</strong>
+          </div>
+          <div id="previewEntry">
+            <textarea id="userPostInput" placeholder="What's on your mind?"></textarea>
+          </div>
+          <button class="save-text-button" title=${singleEntry.modalId}>Save</button>
+        </div>
+      </section>   
     `
   })
   output.innerHTML = userEntryItems.join('');
