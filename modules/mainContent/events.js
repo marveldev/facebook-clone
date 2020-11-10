57b34a3d34d0623c49a02ea16e736ba5aa62019b
Marvel Dev <marvellousalika48@gmail.com>
2020-11-10 18:02:26 +0100

    - add delete item  text -add edit post text

--- modules/mainContent/events.js
+++ modules/mainContent/events.js
@@ -61,7 +61,7 @@ const addPostItemToDb = () => {
       <div class="post-content">
         <div class="post-entry">
           <a href="#">
-            <img src="${bioPhoto ? bioPhoto : 'https://images.pexels.com/photos/5031633/pexels-photo-5031633.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'}"
+            <img src="${bioPhoto ? bioPhoto : 'https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG'}"
             class="profile-photo bio-photo" alt="my profile picture">
           </a>
           <strong class="user-name">${userName ? userName.innerText : 'Jane Doe'}</strong>
@@ -92,7 +92,7 @@ const addPostItemToDb = () => {
         </div>
         <div id="userInfo"> 
           <a href="#">
-            <img src="${bioPhoto ? bioPhoto : 'https://images.pexels.com/photos/5031633/pexels-photo-5031633.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'}"
+            <img src="${bioPhoto ? bioPhoto : 'https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG'}"
             class="profile-photo bio-photo" alt="my profile picture">
           </a>
           <strong id="displayName" class="user-name">${userName ? userName.innerText : 'Jane Doe'}</strong>
@@ -100,11 +100,19 @@ const addPostItemToDb = () => {
         <textarea id="userPostInput" placeholder="What's on your mind?"></textarea>
         <button id="userPostButton" title=${modalId}>Save</button>
       </div>
+      <div class="delete-modal ${postItemId}">
+        <h3>Delete Tweet?</h3>
+        <p>This can't be undone and it will be removed from your post.</p>
+        <button class="cancel-button">Cancel</button>
+        <button class="confirm-button" title=${postItemId}>Delete</button>
+      </div>
     </section>
   `
   userEntryItem += output.innerHTML;
   output.innerHTML = userEntryItem;
   displayPostOptions();
+  editPostText();
+  deleteItemText();
 
   const addItemToIndexDb = {
     postItemId: postItemId,
@@ -116,8 +124,6 @@ const addPostItemToDb = () => {
 }
 
 const displayPostOptions = () => {
-  const output = document.querySelector('.output');
-  const userPostOverlay = document.querySelector('#createPostOverlay');
   const displayOptionsButtons = document.querySelectorAll('.display-options-button');
   for (let index = 0; index < displayOptionsButtons.length; index++) {
     const displayOptionsButton = displayOptionsButtons[index];
@@ -125,8 +131,11 @@ const displayPostOptions = () => {
       displayOptionsButton.nextElementSibling.classList.toggle('show');
     })
   }
+}
 
+const editPostText = () => {
   const editPostButtons = document.querySelectorAll('.edit-text-button')
+  const userPostOverlay = document.querySelector('#createPostOverlay');
   for (let index = 0; index < editPostButtons.length; index++) {
     const editPostButton = editPostButtons[index];
     editPostButton.addEventListener('click', () => {
@@ -134,16 +143,6 @@ const displayPostOptions = () => {
       const editModal = document.querySelector(`#${editModalId}`);
       editModal.style.display = 'block';
       userPostOverlay.style.display = 'block';
-    })
-  }
-
-  const closeEditButtons = document.querySelectorAll('.close-edit-modal')
-  for (let index = 0; index < closeEditButtons.length; index++) {
-    const closeEditButton = closeEditButtons[index];
-    closeEditButton.addEventListener('click', () => {
-      const editPostModal = closeEditButton.parentElement.parentElement;
-      editPostModal.style.display = 'none';
-      userPostOverlay.style.display = 'none';
     })
   }
 
@@ -161,16 +160,56 @@ const displayPostOptions = () => {
     })
   }
 
-  const deletePostButtons = document.querySelectorAll('.delete-post-button')
-  for (let index = 0; index < deletePostButtons.length; index++) {
-    const deletePostButton = deletePostButtons[index];
-    deletePostButton.addEventListener('click', () => {
-      const elementId = deletePostButton.title;
-      const postItem = document.querySelector(`#${elementId}`); 
-      output.removeChild(postItem);
-      deleteEntry('post-item', elementId)
+  const closeEditButtons = document.querySelectorAll('.close-edit-modal')
+  for (let index = 0; index < closeEditButtons.length; index++) {
+    const closeEditButton = closeEditButtons[index];
+    closeEditButton.addEventListener('click', () => {
+      const editPostModal = closeEditButton.parentElement.parentElement;
+      editPostModal.style.display = 'none';
+      userPostOverlay.style.display = 'none';
     })
   }
 }
 
-export { userPostModal, postItemPhoto, displayPostOptions }
+
+const deleteItemText = () => {
+  const deleteButtons = document.querySelectorAll('.delete-post-button');
+  const userPostOverlay = document.querySelector('#createPostOverlay');
+  const output = document.querySelector('.output');
+
+  for (let index = 0; index < deleteButtons.length; index++) {
+    const deleteButton = deleteButtons[index];
+    deleteButton.addEventListener('click', () => {
+      const element = deleteButton.title;
+      const deleteModal = document.querySelector(`.${element}`);
+      console.log(deleteModal);
+      deleteModal.style.display = 'block';
+      userPostOverlay.style.display = 'block';
+    })
+  }
+
+  const cancelButtons = document.querySelectorAll('.cancel-button')
+  for (let index = 0; index < cancelButtons.length; index++) {
+    const cancelButton = cancelButtons[index];
+    cancelButton.addEventListener('click', () => {
+      const deleteModal = cancelButton.parentElement;
+      deleteModal.style.display = 'none';
+      userPostOverlay.style.display = 'none';
+    })
+  }
+
+  const postDeleteButtons = document.querySelectorAll('.confirm-button')
+  for (let index = 0; index < postDeleteButtons.length; index++) {
+    const postDeleteButton = postDeleteButtons[index];
+    postDeleteButton.addEventListener('click', () => {
+      const element = postDeleteButton.title;
+      const postItem = document.querySelector(`#${element}`);
+      output.removeChild(postItem);
+      userPostOverlay.style.display = 'none';
+      deleteEntry('post-item', element);
+    })
+  }
+}
+
+
+export { userPostModal, postItemPhoto, displayPostOptions, editPostText, deleteItemText }
